22c559b0545effe08a7aef5900bf7378b7d53455
mlp7 <mlp7@gatech.edu>
2023-03-17 04:02:28 -0400

    searchbar with dropdown list

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -4,30 +4,50 @@ import axios from 'axios';
 
 function SearchBar (){
 
-    const options = {/* options */};
     let url = "https://raw.githubusercontent.com/ManishLapasi/react-spotify/main/src/components/names_ids.csv";
-    let data = []
+    let songlist = []
+
     axios.get(url)
         .then(function (response) {
-            console.log(response)
-            data = response.data.split("\n");
-        })
+            let data = response.data.split("\n");
+            for (let i=0; i<data.length;i++) {
+                let [id, ...name] = data[i].split(",");
+                let trackname = name.join(",");
+                songlist.push({id:id, name:trackname});
+            }
+        })    
+
 
     const [song, setSong] = useState('');
+    const [filteredlist, setFilteredList] = useState([]);
 
-    const handleClick = () => {
-        console.log(song);
+    const handleSearch = (event) => {
+        let val = event.target.value.toLowerCase();
+        //console.log("searching", event.target.value);
+        let searchedList = [];
+        for (let i in songlist) {
+            if (songlist[i].name.toLowerCase().includes(val)){searchedList.push(songlist[i])}
+            if (searchedList.length>99) {break;}
+        }
+        //console.log(searchedList);
+        setFilteredList(searchedList);
     }
 
-    const handleSearch = () => {
-        console.log("searching");
+    const handleSelect = (event) => {
+        setSong(event.target.value);
     }
 
 
     return (
         <div className="searchbar">
-            <input type="text" placeholder="Search for a song!" value={song} onChange={handleSearch}/>
-            <button type='submit' onClick={handleClick}></button>
+            <input type="text" placeholder="Search for a song!" onChange={handleSearch}/>
+            <select onChange={handleSelect}>
+                {filteredlist.map((ele) => (
+                    <option key={ele.id} value={ele.id}>
+                        {ele.name}
+                    </option>
+                ))}
+            </select>
         </div>
     );
 }
