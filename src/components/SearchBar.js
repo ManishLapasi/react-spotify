28d2a2053d1fcd1d0e26187c8ca781e8ffe46d2f
mlp7 <mlp7@gatech.edu>
2023-03-17 00:28:54 -0400

    ini commit

---
+++ src/components/SearchBar.js
@@ -0,0 +1,11 @@
+import './SearchBar.css';
+
+function SearchBar (){
+    return (
+        <div className="searchbar">
+            <input type="text" placeholder="Search for a song!" />
+        </div>
+    );
+}
+
+export default SearchBar;
