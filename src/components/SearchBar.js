723c50bbcad168fc3f85224b8257aa9e5e2dcded
mlp7 <mlp7@gatech.edu>
2023-03-17 01:56:26 -0400

    added a few files

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,26 @@
+import { useState } from 'react';
 import './SearchBar.css';
+import "./songlist";
 
 function SearchBar (){
+
+    console.log(songarr[0]);
+
+    const [song, setSong] = useState('');
+
+    const handleClick = () => {
+        console.log(song);
+    }
+
+    const handleSearch = () => {
+        console.log("searching");
+    }
+
+
     return (
         <div className="searchbar">
-            <input type="text" placeholder="Search for a song!" />
+            <input type="text" placeholder="Search for a song!" value={song} onChange={handleSearch}/>
+            <button type='submit' onClick={handleClick}></button>
         </div>
     );
 }
