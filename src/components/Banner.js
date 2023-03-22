960177f307fcbf36407d2d1d16a8d474457d223c
mlp7 <mlp7@gatech.edu>
2023-03-22 18:32:51 -0400

    css changes

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -5,7 +5,7 @@ import React from "react";
 export default function Banner(props) {
     return (
         <div className="banner">
-            {props.songId==="" ? (<span>{props.tagline}</span>) : (<div><span>For the song </span><a href={"https://open.spotify.com/track/"+props.songId} className="linker" target="_blank" rel="noopener noreferrer">{props.songName}</a><span> <div className="embedder"><Spotify wide height={"100px"}  link={"https://open.spotify.com/track/"+props.songId}></Spotify></div> this app searched through {props.numSongs} songs!</span></div>)}
+            {props.songId==="" ? (<div><span>{props.tagline}</span></div>) : (<div><span>For the song </span><a href={"https://open.spotify.com/track/"+props.songId} className="linker" target="_blank" rel="noopener noreferrer">{props.songName}</a><span> <div className="embedder"><Spotify wide height={"100px"}  link={"https://open.spotify.com/track/"+props.songId}></Spotify></div> this app searched through {props.numSongs} songs!</span></div>)}
         </div>
     )
 }
